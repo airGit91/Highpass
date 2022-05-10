90265b9d92a0c4bb3bb42419866949868d6a2de2
Nik <airgit1991@rambler.ru>
2022-05-10 17:29:19 +0300

    +

--- gulpfile.js
+++ gulpfile.js
@@ -1,115 +1,223 @@
 const {
     src,
     dest,
+    parallel,
     series,
     watch
-} = require('gulp')
-const concat = require('gulp-concat')
-const autoprefixer = require('gulp-autoprefixer')
-const sass = require('gulp-sass')(require('sass'))
-const csso = require('gulp-csso')
-const htmlmin = require('gulp-htmlmin')
-const babel = require('gulp-babel')
-const uglify = require('gulp-uglify-es').default
-const sourcemaps = require('gulp-sourcemaps')
-const svgSprite = require('gulp-svg-sprite')
-const imagemin = require('gulp-imagemin')
-const webp = require('gulp-webp')
-const del = require('del')
-const sync = require('browser-sync').create()
+} = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify-es').default;
+const sourcemaps = require('gulp-sourcemaps');
+const notify = require('gulp-notify');
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const webp = require('gulp-webp');
+const sync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
+const svgSprite = require('gulp-svg-sprite');
+const del = require('del');
+const browserSync = require('browser-sync');
+const webpackStream = require('webpack-stream');
+const webpack = require('webpack');
 
-
-const html = () => {
-    return src('src/**.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-        }))
-        .pipe(dest('build/src'))
+const clean = () => {
+    return del(['dev/*'])
 }
 
-const scss = () => {
-    return src('src/styles/scss/**.scss')
+const htmlInclude = () => {
+    return src(['./index.html'])
+        .pipe(fileinclude({
+            prefix: '@',
+            basepath: '@file',
+        }))
+        .pipe(dest('./dev'))
+        .pipe(browserSync.stream())
+}
+
+const styles = () => {
+    return src('./src/styles/scss/**.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(sass({
+            outputStyle: 'expanded',
+        }).on('error', notify.onError()))
+        .pipe(concat('style.css'))
+        .pipe(rename({
+            suffix: '.min',
+        }))
         .pipe(autoprefixer({
             grid: true,
             cascade: false,
         }))
-        .pipe(csso())
-        .pipe(concat('style.css'))
-        .pipe(sourcemaps.write())
-        .pipe(dest('build/src/styles/css'))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./dev/src/styles/css'))
+        .pipe(browserSync.stream())
+}
+
+const imgToDev = () => {
+    return src(['./src/images/**/**.jpg'])
+        .pipe(dest('./dev/src/images'))
+}
+
+const svgToDev = () => {
+    return src(['./src/images/**.svg'])
+        .pipe(dest('./dev/src/images'))
+}
+
+const fontsToDev = () => {
+    return src(['./src/fonts/**'])
+        .pipe(dest('./dev/src/fonts'))
 }
 
 const scripts = () => {
-    return src('src/scripts/**.js')
+    return src('./src/scripts/index.js')
+        // .pipe(webpackStream({
+        //     output: {
+        //         filename: 'index.js',
+        //     },
+        //     module: {
+        //         rules: [{
+        //             test: /\.m?js$/,
+        //             exclude: /node_modules/,
+        //             use: {
+        //                 loader: 'babel-loader',
+        //                 options: {
+        //                     presets: [
+        //                         ['@babel/preset-env', {
+        //                             targets: "defaults"
+        //                         }]
+        //                     ]
+        //                 }
+        //             }
+        //         }]
+        //     }
+        // }))
         .pipe(sourcemaps.init())
         .pipe(babel({
-            presets: ["@babel/preset-env"]
+            presets: ['@babel/env']
         }))
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(dest('build/src/scripts'));
+        .pipe(uglify({
+            toplevel: true,
+        }).on('error', notify.onError()))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./dev/src/scripts/'))
+        .pipe(browserSync.stream())
 }
 
-const svgSprites = () => {
-    return src('src/images/**/*.svg')
-        .pipe(svgSprite({
-            mode: {
-                stack: {
-                    sprite: '../sprite.svg'
-                }
-            }
-        }))
-        .pipe(dest('build/src/images'))
-}
-
-
-const clear = () => {
-    return del('dist')
-}
-
-const serve = () => {
+const watchFiles = () => {
     sync.init({
-        server: './dist',
+        server: './dev',
     })
-    watch('src/**.html', series(html)).on('change', sync.reload)
-    watch('src/styles/scss/**.scss', series(html)).on('change', sync.reload)
+    watch('./index.html', htmlInclude);
+    watch('./src/styles/scss/**.scss', styles);
+    watch('./src/images/**/**.jpg', imgToDev);
+    watch('./src/images/**.svg', svgToDev);
+    watch('./src/images/**.svg', fontsToDev);
+    watch('./src/scripts/**/**.js', scripts);
 }
 
+exports.fileInclude = htmlInclude;
+exports.styles = styles;
+exports.watchFiles = watchFiles;
 
 
-exports.html = html
-exports.scss = scss
-exports.scripts = scripts
-// exports.build = series(clear, scss, html)
-exports.build = series(html, scss, scripts, svgSprites)
-// exports.serve = series(clear, scss, html, serve)
-exports.clear = clear
+exports.default = series(clean, parallel(htmlInclude, scripts, imgToDev, svgToDev, fontsToDev), styles, watchFiles);
 
+//BUILD//
 
-/* gulp Dev */
+const cleanBuild = () => {
+    return del(['build/*'])
+}
 
-const scssDev = () => {
-    return src('src/styles/scss/**.scss')
-        .pipe(sass())
+const htmlBuild = () => {
+    return src(['./index.html'])
+        .pipe(fileinclude({
+            prefix: '@',
+            basepath: '@file',
+        }))
+        .pipe(dest('./build'))
+        .pipe(browserSync.stream())
+}
+
+const htmlMin = () => {
+    return src('./**.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+        }))
+        .pipe(dest('build'))
+}
+
+const stylesBuild = () => {
+    return src('./src/styles/scss/**.scss')
+        .pipe(sass({
+            outputStyle: 'expanded',
+        }).on('error', notify.onError()))
+        .pipe(concat('style.css'))
+        .pipe(rename({
+            suffix: '.min',
+        }))
         .pipe(autoprefixer({
             grid: true,
             cascade: false,
         }))
-        .pipe(csso())
-        .pipe(concat('style.css'))
-        .pipe(dest('src/styles/css'))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(dest('./build/src/styles/css'))
 }
 
-const dev = () => {
-    sync.init({
-        server: './src',
-    })
-    watch('src/**.html').on('change', sync.reload)
-    watch('src/styles/scss/**.scss', series(scssDev)).on('change', sync.reload)
+const scriptsBuild = () => {
+    return src('./src/scripts/index.js')
+        // .pipe(webpackStream({
+        //     output: {
+        //         filename: 'index.js',
+        //     },
+        //     module: {
+        //         rules: [{
+        //             test: /\.m?js$/,
+        //             exclude: /node_modules/,
+        //             use: {
+        //                 loader: 'babel-loader',
+        //                 options: {
+        //                     presets: [
+        //                         ['@babel/preset-env', {
+        //                             targets: "defaults"
+        //                         }]
+        //                     ]
+        //                 }
+        //             }
+        //         }]
+        //     }
+        // }))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify({
+            toplevel: true,
+        }).on('error', notify.onError()))
+        .pipe(dest('./build/src/scripts/'))
 }
 
-exports.scssDev = scssDev
-exports.dev = series(scssDev, dev) //для разработки
+const imgToBuild = () => {
+    return src(['./src/images/**/**.jpg'])
+        .pipe(dest('./build/src/images'))
+}
+
+const svgToBuild = () => {
+    return src(['./src/images/**.svg'])
+        .pipe(dest('./build/src/images'))
+}
+
+const fontsToBuild = () => {
+    return src(['./src/fonts/**'])
+        .pipe(dest('./build/src/fonts'))
+}
+
+exports.build = series(cleanBuild, parallel(htmlBuild, scriptsBuild, imgToBuild, svgToBuild, fontsToBuild), htmlMin, stylesBuild);
